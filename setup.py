bd4b38d8b31921d08ecde30d9126fe5f9255d9ed
saber Boukhriss <saber@securas.lab>
2024-11-02 02:01:06 +0100

    Licence change to Mit and README fix

--- setup.py
+++ setup.py
@@ -3,29 +3,40 @@ from setuptools import setup, find_packages
 setup(
     name='Bluto',
     version='2.4.18',
-    maintainer=='ProjectResurrect',
-    maintainer_email=='contact@boukhrisssaber.tn',
+    author='Darryl Lane',
+    maintainer='ProjectResurrect',
+    maintainer_email='contact@boukhrisssaber.tn',
     url='https://github.com/ProjectResurrect/Bluto',
-    packages=['Bluto'],
+    packages=find_packages(),  # Automatically finds all packages and subpackages
     include_package_data=True,
     license='LICENSE.txt',
-    description='''
-    DNS Recon | Brute Forcer | DNS Zone Transfer | DNS Wild Card Checks
-    DNS Wild Card Brute Forcer | Email Enumeration | Staff Enumeration
-    Compromised Account Checking''',
-    long_description_content_type='text/markdown',
+    description=(
+        "DNS Recon | Brute Forcer | DNS Zone Transfer | DNS Wild Card Checks | "
+        "DNS Wild Card Brute Forcer | Email Enumeration | Staff Enumeration | "
+        "Compromised Account Checking"
+    ),
     long_description=open('README.md').read(),
-    scripts=['Bluto/bluto'],
+    long_description_content_type='text/markdown',
     install_requires=[
-    	"docopt",
+        "docopt",
         "dnspython",
         "termcolor",
-        "BeautifulSoup4",
+        "beautifulsoup4",
         "requests[security]",
         "pythonwhois",
         "lxml",
         "oletools",
-        "pdfminer.six"
+        "pdfminer.six"  # Updated to pdfminer.six as pdfminer is outdated
     ],
+    python_requires='>=3.6',  # Requires Python 3.6 or higher
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Use the appropriate license if different
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bluto=Bluto.bluto:main',  # Allows users to run 'bluto' from the CLI
+        ],
+    },
 )
-
